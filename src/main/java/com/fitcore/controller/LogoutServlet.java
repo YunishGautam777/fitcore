3e22de197197edb4a7ab836036b4daf54af81b74
Shusham_Parajuli <shantiramparajuli@gmail.com>
2026-05-14 13:36:09 +0545

    Refactored LogoutServlet: expanded imports, renamed variables, extracted helper methods

--- src/main/java/com/fitcore/controller/LogoutServlet.java
+++ src/main/java/com/fitcore/controller/LogoutServlet.java
@@ -1,15 +1,37 @@
 package com.fitcore.controller;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        HttpSession s = req.getSession(false);
-        if (s != null) s.invalidate();
-        res.sendRedirect(req.getContextPath() + "/login.jsp?loggedOut=1");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        terminateUserSession(request);
+        redirectToLoginPage(request, response);
+    }
+
+    private void terminateUserSession(HttpServletRequest request) {
+        HttpSession currentSession = request.getSession(false);
+        boolean sessionExists = (currentSession != null);
+
+        if (sessionExists) {
+            currentSession.invalidate();
+        }
+    }
+
+    private void redirectToLoginPage(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        String contextPath = request.getContextPath();
+        String loginUrl = contextPath + "/login.jsp?loggedOut=1";
+        response.sendRedirect(loginUrl);
     }
 }
